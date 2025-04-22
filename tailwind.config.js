af3c5394805dce992eff5b8970a4db8c1fdeb8bf
rkhael <renatokhael@gmail.com>
2025-04-21 22:03:27 -0300

    feat: create components

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        primary: '#4f46e5',
+        secondary: '#0284c7',
+        dark: {
+          bg: '#121212',
+          card: '#1e1e1e',
+          text: '#f3f4f6',
+        },
+        light: {
+          bg: '#f9fafb',
+          card: '#ffffff',
+          text: '#1f2937',
+        },
+      },
+    },
+  },
+  plugins: [],
+} 
